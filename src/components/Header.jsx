57ac7f36308092af9ec88a8e156f1c8610879f6a
amirthenew <ah.abshari@hotmail.com>
2023-01-29 22:59:56 +0330

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from '../components/Header.module.css'
 
 const Header = ()=>{
 
-return(<h2>
+return(<h2 className={styles.header}>
     to do app
     </h2>)
 
