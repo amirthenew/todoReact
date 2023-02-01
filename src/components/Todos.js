f734938ff9b2734266513b19182c8f933dd68742
amirthenew <ah.abshari@hotmail.com>
2023-02-01 14:22:46 +0330

    delete task

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,39 +6,46 @@ import Header from './Header';
 
 const trashIcon = <FontAwesomeIcon icon={faTrash}/>
 const Todos = () => {
-
-    const [message ,setMessage] = useState('')
-    
+    const [message ,setMessage] = useState('') 
 const changHandler=(event) =>{
     setMessage(event.target.value)
 } 
-
 const [tasks,setTask]=useState([])
-
 const taskRemover=()=>{
-setTask({})
+setTask([])
+}
+const taskHandler=()=>{
+setTask([...tasks,{message}])
+setMessage("")
+}  
+let deleteTask = (index)=>{
+
+    const newTask = [...tasks]
+    console.log(newTask);
+    newTask.splice(index,1)
+    setTask(newTask)
 }
 
-const taskHandler=()=>{
-setTask([...tasks,{message:message,id:tasks.id}])
-setMessage("")
-}    
+
+
 return ( 
-        <div className={styles.box}>
-        <Header/>
-        <input type="text" value={message} onChange={changHandler} />
-      <button onClick={taskHandler}>Add</button>
-      <button onClick={taskRemover}>Clear</button>
-      {tasks.map((task)=>(
-        <li>{task.message}<i>{trashIcon}</i></li>
-        
-             ))}
-             {console.log(tasks)}
-        </div>
-    
-    );
+    <div className={styles.box}>
+    <Header/>
+    <input type="text" value={message} onChange={changHandler} />
+  <button onClick={taskHandler}>Add</button>
+  <button onClick={taskRemover}>Clear</button>
+  {tasks.map((task)=>(
+    <li key={task.id}>{task.message}<i onClick={deleteTask}>{trashIcon}</i></li> 
+         ))}
+         {console.log(tasks)}
+    </div>
+)  
 }
- 
+
+
 export default Todos;
 
 
+
+
+// <li key={2}>{task.message}<i onClick={console.log('aasd')}>{trashIcon}</i></li>
