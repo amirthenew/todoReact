90748decb0d12043398f08ebe1809cb4e7c029bf
amirthenew <ah.abshari@hotmail.com>
2023-02-02 02:12:33 +0330

    confused

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styles from '../components/Todos.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBackspace, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faBackspace, faTrash,faPlus } from '@fortawesome/free-solid-svg-icons'
 import Header from './Header';
 
 const trashIcon = <FontAwesomeIcon icon={faTrash}/>
+const plusIcon  = <FontAwesomeIcon icon={faPlus}/>
 const Todos = () => {
-    const [message ,setMessage] = useState('') 
+const [message ,setMessage] = useState('') 
 const changHandler=(event) =>{
     setMessage(event.target.value)
 } 
@@ -14,26 +15,20 @@ const [tasks,setTask]=useState([])
 const taskRemover=()=>{
 setTask([])
 }
-const taskHandler=()=>{
-setTask([...tasks,{message}])
+const addTask=()=>{
+setTask([...tasks])
 setMessage("")
 }  
 let deleteTask = (index)=>{
-
     const newTask = [...tasks]
-    console.log(newTask);
     newTask.splice(index,1)
     setTask(newTask)
 }
-
-
-
 return ( 
     <div className={styles.box}>
     <Header/>
     <input type="text" value={message} onChange={changHandler} />
-  <button onClick={taskHandler}>Add</button>
-  <button onClick={taskRemover}>Clear</button>
+  <button className={styles.addBtn} onClick={addTask} style={{'padding-left':'0.5rem','padding-right':'0.5rem'}}>{plusIcon}</button>
   {tasks.map((task)=>(
     <li key={task.id}>{task.message}<i onClick={deleteTask}>{trashIcon}</i></li> 
          ))}
