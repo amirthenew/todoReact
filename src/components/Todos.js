787401d1f98d5e70be1645f15296df42c8a692f4
amirthenew <ah.abshari@hotmail.com>
2023-02-25 22:49:16 +0330

    new bugs appeard !

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import styles from '../components/Todos.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBackspace, faTrash,faPlus } from '@fortawesome/free-solid-svg-icons'
+import {faTrash,faPlus } from '@fortawesome/free-solid-svg-icons'
 import Header from './Header';
 
 
@@ -9,17 +9,12 @@ const faTrashs = <FontAwesomeIcon icon={faTrash}/>
 const plusIcon  = <FontAwesomeIcon icon={faPlus}/>
 const Todos = () => {
 
-
   useEffect(()=>{
    getTask()
     },[])
-
-  
-
-    
-const [text ,setText] = useState('') 
+   
+const [inputValue ,setInputValue] = useState('') 
 const [tasks,setTask]=useState([])
-const [status,setStatus]=useState(false)
 
 
 const getTask = ()=>{
@@ -31,58 +26,59 @@ setTask([...oldTasks])
 const saveTask = ()=> {
 
 const todoItem = [...tasks]
-todoItem.push(text)
+todoItem.push(inputValue)
 localStorage.setItem('tasks',JSON.stringify(todoItem))
 }
 
 const changHandler=(event) =>{
-    setText(event.target.value)} 
+    setInputValue(event.target.value)
+  } 
 
 
 const addTask=()=>{
-setTask([...tasks,text])
+setTask([...tasks,{taskName:inputValue,completed : false}])
 saveTask()
-setText("")
+setInputValue('')
 }  
 
-const toggleClass = ()=>{
-  
-  setStatus(!status)
-
-}
 
 const clearAll = ()=>{
 setTask([])
 localStorage.removeItem('tasks')
 }
 
-const removeTask = index=>{
-  
-// setTask((oldValues)=>{
-//   console.log(oldValues);
-//   localStorage.setItem('tasks',JSON.stringify(oldValues))
-//  return oldValues.filter(task=>task !== value)
-
-// })
-tasks.splice(index,1)
-setTask([...tasks])
-console.log(tasks);
+const removeTask = (index)=>{
+const newTasks =[...tasks]
+newTasks.splice(index,1)
+setTask(newTasks)
 localStorage.setItem('tasks',JSON.stringify(tasks))
 
+}
+
+const taskDone = (index)=>{
+  const newTasks = [...tasks]
+  newTasks[index].completed=true
+  setTask(newTasks)
 }
 
 return ( 
     <div className={styles.box}>
     <Header/>
-    <input className={styles.placeholder} placeholder='enter your task here ...' type="text" value={text} onChange={changHandler} />
+    <input 
+    className={styles.placeholder} 
+    placeholder='enter your task here ...' 
+    type="text" 
+    value={inputValue} 
+    onChange={changHandler} />
     <button className={styles.addBtn} onClick={addTask} 
     style={{padding:'0 .5rem'}}>{plusIcon}</button>
     <button className={styles.trash} onClick={clearAll}>{faTrashs}</button>
-  {tasks.map((task,id)=> 
+  {tasks.map((task,index)=> 
     <div 
-    key={id} 
-  className={styles.todo}><span className={status ? styles.completed : null} 
-  onClick={toggleClass}>{id+1} : {task}</span>
+    key={index} 
+  style={{textDecoration : task.completed ? 'line-through' : 'null'}}>
+  
+  <span onClick={()=>taskDone(index)} >{index+1} : {task.taskName}</span>
   <i className={styles.trashIcon} onClick={()=>removeTask(task)}>{faTrashs}</i>
 
   </div>)}
